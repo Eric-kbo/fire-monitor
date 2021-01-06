c10fa33c2d16e7e6aa81b85b660a392f9cfe9764
v_xbzzhou <v_xbzzhou@tencent.com>
2021-01-06 18:21:01 +0800

    提交

--- web/src/views/login/login.vue
+++ web/src/views/login/login.vue
@@ -7,19 +7,58 @@
         :form="form"
         @submit="login"
     >
-      <md-field md-inline>
-        <a-input placeholder="请输入账号" v-model="form.account"/>
-      </md-field>
-      <md-field md-inline>
-        <a-input-password
-            placeholder="请输入密码"
-            v-model="form.password"
-            @enter="login"
-        />
-      </md-field>
-      <div>
-        <a-button type="primary" block @click="login">登录</a-button>
-      </div>
+      <a-tabs default-active-key="1"
+              :tabBarStyle="{ textAlign: 'center', borderBottom: 'unset' }"
+      >
+        <a-tab-pane key="1" tab="账号密码登陆">
+          <a-form-item>
+            <a-input placeholder="请输入账号" v-model="form.account">
+              <a-icon slot="prefix" type="user" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-input-password
+                placeholder="请输入密码"
+                v-model="form.password"
+                @enter="login"
+            >
+              <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+            </a-input-password>
+          </a-form-item>
+          <div>
+            <a-button type="primary" block @click="login">登录</a-button>
+          </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="手机号登陆" force-render>
+          <a-form-item>
+            <a-input size="large" type="text" placeholder="请输入手机号"
+                     v-decorator="['mobile', {rules: [{ required: true, pattern: /^1[34578]\d{9}$/, message: '111'}], validateTrigger: 'change'}]">
+              <a-icon slot="prefix" type="mobile" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+            </a-input>
+          </a-form-item>
+
+          <a-row :gutter="16">
+            <a-col class="gutter-row" :span="16">
+              <a-form-item>
+                <a-input size="large" type="text" placeholder="输入验证码"
+                         v-decorator="['captcha', {rules: [{ required: true, message: '22' }], validateTrigger: 'blur'}]">
+                  <a-icon slot="prefix" type="mail" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+                </a-input>
+              </a-form-item>
+            </a-col>
+            <a-col class="gutter-row" :span="8">
+              <!--              <a-button-->
+              <!--                  class="getCaptcha"-->
+              <!--                  tabindex="-1"-->
+              <!--                  :disabled="state.smsSendBtn"-->
+              <!--                  @click.stop.prevent="getCaptcha"-->
+              <!--                  v-text="!state.smsSendBtn && $t('user.register.get-verification-code') || (state.time+' s')"-->
+              <!--              ></a-button>-->
+              <a-button type="primary" block @click="login">登录</a-button>
+            </a-col>
+          </a-row>
+        </a-tab-pane>
+      </a-tabs>
     </a-form>
   </div>
 </template>
@@ -77,8 +116,10 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
   .login-form {
     min-width: 200px;
+
     .md-input {
       border-bottom: 1px solid rgba($color: #000000, $alpha: 0.5);
     }
