1a7eee90f3672ab3441d9dda1c161f5aaeee8604
sltruman <sl.truman@live.com>
2021-01-31 16:06:24 +0800

    告警接口返回状态数据

--- services/gather_of_chntek_db.py
+++ services/gather_of_chntek_db.py
@@ -58,50 +58,74 @@ def gather(token,ids,today=datetime.datetime.now()):
     r = requests.get(f'http://iot.chntek.com:3410/api/Terminal/RealTimeData?searchDate={today.date()}',headers={'Authorization': token})
     r = r.json()
 
+    statues = {}
+    warnings = {}
+    primaries = {}
+
     for w in r['val']:
         tid = w['ProductTerId']
         os.makedirs(f'db/devices/{tid}',exist_ok=True)
         os.makedirs(f'db/devices/{tid}/status',exist_ok=True)
         os.makedirs(f'db/devices/{tid}/warnings',exist_ok=True)
 
+        try:   
+            if tid not in primaries:
+                with open(f'db/devices/{tid}/primary.json','r') as f:
+                    primaries[tid] = json.load(f)
+            if tid not in statues:
+                statues[tid] = {}
+                with open(f'db/devices/{tid}/status/{today.date()}.json','r') as f:
+                    for status in json.load(f):
+                        statues[tid][status['time']] = status
+        except FileNotFoundError as e:
+            print(e)
+            continue
+
+        if tid not in warnings:
+            warnings[tid] = []
+     
+        primaries[tid]['id'] = tid
+        primaries[tid]['longitude'] = w['Longitude'] if w['Longitude'] else primaries[tid]['longitude']
+        primaries[tid]['latitude'] = w['Latitude'] if w['Latitude'] else primaries[tid]['latitude'],
+        primaries[tid]['city'] = w['Prefecturecity'] if w['Prefecturecity'] else primaries[tid]['city'],
+        primaries[tid]['county'] = w['Distriancounty'] if w['Distriancounty'] else primaries[tid]['county'],
+        primaries[tid]['location'] = w['Customerunit'] if w['Customerunit'] else primaries[tid]['location'],
+        primaries[tid]['type'] = primaries[tid]['type'] if 'type' in primaries[tid] else 'firehydrant'  #设备类型 firehydrant：消防栓，pressure：无线压力表，cylinders：消防气瓶
+        
+        if not w['WarnName']: continue
+        w['WarnName'] = w['WarnName'].replace('、水质报警','')
+        w['WarnName'] = w['WarnName'].replace('水质报警','')
+        s = statues[tid]
+        t = w['MonitorsTime']
+
         try:
-            print(f'gather db/devices/{tid}/warnings/{today.date()}.json',end=' ')
-            with open(f'db/devices/{tid}/primary.json','r+') as f:
-                primary_later = json.load(f)
-                primary = {
-                    'id':tid,
-                    'longitude':w['Longitude'] if w['Longitude'] else primary_later['longitude'],
-                    'latitude': w['Latitude'] if w['Latitude'] else primary_later['latitude'],
-                    'city': w['Prefecturecity'] if w['Prefecturecity'] else primary_later['city'],
-                    'county': w['Distriancounty'] if w['Distriancounty'] else primary_later['county'],
-                    'location': w['Customerunit'] if w['Customerunit'] else primary_later['location'],
-                    'type': primary_later['type'] if 'type' in primary_later else 'firehydrant'  #设备类型 firehydrant：消防栓，pressure：无线压力表，cylinders：消防气瓶
-                }
+            warnings[tid].append({
+                'hydraulic_pressure': s[t]['hydraulic_pressure'],
+                'temperature':        s[t]['temperature'],
+                'energy':             s[t]['energy'],
+                'time':               t,
+                'signal_intensity':   s[t]['signal_intensity'],          
+                'conductivity':       s[t]['conductivity'],
+                'type': w['WarnName'],
+                'time': w['MonitorsTime'],
+                'location': w['Prefecturecity'] + w['Distriancounty'] + w['Customerunit']
+            })
+        except KeyError as e:
+            print(e)
+            continue
 
-                f.seek(0)
-                f.truncate()
-                f.write(json.dumps(primary,indent=4,ensure_ascii=False))
+    for tid in primaries:
+        p = primaries[tid]
+        print(f'update db/devices/{tid}/primary.json')
+        with open(f'db/devices/{tid}/primary.json','w') as f:
+            f.write(json.dumps(p,indent=4,ensure_ascii=False))
 
-            with open(f'db/devices/{tid}/warnings/{today.date()}.json','r+') as f:
-                warnings = json.load(f)
-                if not w['WarnName']: continue
-                w['WarnName'] = w['WarnName'].replace('、水质报警','')
-                w['WarnName'] = w['WarnName'].replace('水质报警','')
-
-                warnings.append({
-                    'type': w['WarnName'],
-                    'time': w['MonitorsTime'],
-                    'location': w['Prefecturecity'] + w['Distriancounty'] + w['Customerunit']
-                })
-                f.seek(0)
-                f.truncate()
-                f.write(json.dumps(warnings,indent=4,ensure_ascii=False))
-        except FileNotFoundError as e: 
-            pass
-        except TypeError as e:
-            pass
-        finally:
-            print()
+    for tid in warnings:
+        w = warnings[tid]
+        
+        print(f'gather db/devices/{tid}/warnings/{today.date()}.json')
+        with open(f'db/devices/{tid}/warnings/{today.date()}.json','w') as f:
+            f.write(json.dumps(w,indent=4,ensure_ascii=False))
 
 print('program [recent days]')
 import sys
